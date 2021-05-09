7e91e7d9bad8f80b4d42a9c344c7d5463a35c7c5
Bilal <b.umrani75@gmail.com>
2021-05-10 02:17:43 +0500

    now sending token in response when user signin

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -3,69 +3,74 @@ const router = express.Router(); // here we will use all routes like get and pos
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const bcrypt = require('bcryptjs');
+const jt = require('jsonwebtoken');
+const { JT_SECRET } = require('../keys');
 
 router.get('/', (req, res) => {
 
     res.send("hello this is main directory");
 })
 
+// route for signup
+router.post('/signup', (req, res) => {
 
-// router.post('/signup', (req, res) => {
-
-//     const { name, email, password } = req.body;
-//     if (!email || !password || !name) {
-//         return res.status(422).json({ "error": "Please add all the fields" })
-//     }
-//     res.json({ "message": "successfully posted the data" })
-//     User.findOne({ email: email }).then((savedUser) => {
-//         if (savedUser) {
-//             return res.status(422).json({ "error": "User already exist with that email" })
-//         }
-//         bcrypt.hash(password, 11)
-//             .then(hashedpassword => {
-
-//                 const user = new User({
-//                     email,
-//                     password:hashedpassword,
-//                     name
-//                 })
-
-//                 user.save().then((user) => {
-//                     res.json({ message: "saved successfully" })
-//                 }).catch(err => { console.log(err) })
-//             })
-
-
-//     }).catch(err => { console.log(err) })
-// })
-
-
-
-router.post('/signin',(req,res)=>{
-    const {email,password} = req.body;
-    if(!email || !password){
-        res.status(422).json({"error":"password or email is missing"});
+    const { name, email, password } = req.body;
+    if (!email || !password || !name) {
+        return res.status(422).json({ "error": "Please add all the fields" })
     }
-User.findOne({email:email}).then((savedUser)=>{
-    if(!savedUser)
-    {
-        res.status(422).json({"error":"Invalid password or Eamil"});
-    }
-    bcrypt.compare(password,savedUser.password).then(matched=>{
-        if(matched){
-            res.json({"message":"successfully signed in"})
+    res.json({ "message": "successfully posted the data" })
+    User.findOne({ email: email }).then((savedUser) => {
+        if (savedUser) {
+            return res.status(422).json({ "error": "User already exist with that email" })
         }
-        else{
-            res.status(422).json({"error":"Invalid password or Eamil"});
-        }
-    }).catch(err=>{console.log(err)})
+        bcrypt.hash(password, 11)
+            .then(hashedpassword => {
+
+                const user = new User({
+                    email,
+                    password: hashedpassword,
+                    name
+                })
+
+                user.save().then((user) => {
+                    res.json({ message: "saved successfully" })
+                }).catch(err => { console.log(err) })
+            })
+
+
+    }).catch(err => { console.log(err) })
 })
 
 
-// testing
+//route for signin
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(422).json({ "error": "password or email is missing" });
+    }
+    User.findOne({ email: email }).then((savedUser) => {
+        if (!savedUser) {
+            res.status(422).json({ "error": "Invalid password or Eamil" });
+        }
+        bcrypt.compare(password, savedUser.password).then(matched => {
+            if (matched) {
+                //res.json({ "message": "successfully signed in" })
+                const token = jt.sign({_id:savedUser._id},JT_SECRET);
+                res.json({token});
+            }
+            else {
+                 res.status(422).json({"error":"Invalid password or Eamil"});
+                
 
-// "email": "nomi@ibsSucker.com",
-// "password":"wawawawa"
+            }
+        }).catch(err => { console.log(err) })
+    })
+
+
+    // testing
+
+    // "email": "nomi@ibsSucker.com",
+    // "password":"wawawawa"
 
 
 })
