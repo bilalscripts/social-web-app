4c0d4cbbafa765d3ed7f5983e1706ba53c483857
Bilal <b.umrani75@gmail.com>
2021-06-01 00:53:36 +0500

    ok

--- frontend/src/components/screens/Card.jsx
+++ frontend/src/components/screens/Card.jsx
@@ -119,7 +119,7 @@ const Card = (props) => {
               <div className='float-end'>
                 {(props.postedById === state._id) && <button className='btn btn-transparent' onClick={() => { deletePost(props.id) }}><DeleteRoundedIcon /></button>}
               </div>
-              <Link to={ (props.postedById !== state._id) ?: `profile/${props.postedById}` `profile`} className="btn">{props.postedBy}</Link>
+              <Link to={ (props.postedById !== state._id) ?`profile/${props.postedById}` :`profile` } className="btn">{props.postedBy}</Link>
               <p className='my-3'>{props.body}</p>
             </div>
             <div className='image border-bottom p-2'>
